7789e1f7b3535753f9956039088042ef0be8d05b
tsi-annakarenina <karenina.os@brins.co.id>
2022-06-04 16:22:50 +0700

    1.ui changes on home menu

--- app/src/main/java/com/nextint/learncrypto/app/features/overview/HomeFragment.kt
+++ app/src/main/java/com/nextint/learncrypto/app/features/overview/HomeFragment.kt
@@ -23,6 +23,8 @@ import com.nextint.learncrypto.app.features.overview.presentation.OverviewViewMo
 import com.nextint.learncrypto.app.features.search.presentation.SearchFragment
 import com.nextint.learncrypto.app.features.ui.dialog.DialogModel
 import com.nextint.learncrypto.app.features.utils.UtilitiesFunction
+import com.nextint.learncrypto.app.features.utils.cornerImage
+import com.nextint.learncrypto.app.features.utils.loadImage
 import com.nextint.learncrypto.app.util.KEY_BUNDLE_MODEL_DIALOG
 import com.nextint.learncrypto.app.util.TAG_DIALOG
 
@@ -126,40 +128,52 @@ class HomeFragment : BaseFragment<OverviewViewModel>() {
     }
 
 
-    @SuppressLint("UseCompatLoadingForDrawables")
     private fun displayView()
     {
         with(_getBindingHomeFragment?.menuConcept!!){
             textViewTitle.text = getString(R.string.menu_concept)
-            textViewNumber.text = getString(R.string.menu_number_01)
+            imageMenu.clipToOutline = true
+            imageMenu.loadImage("https://images.unsplash.com/photo-1621932953986-15fcf084da0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZG9nZWNvaW58ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60")
             cardMenu.setOnClickListener()
             {
-                it.background = context?.getDrawable(R.color.primary)
+                //it.background = context?.getDrawable(R.color.primary)
                 UtilitiesFunction.replaceFragment(parentFragmentManager, ConceptFragment())
             }
         }
         with(_getBindingHomeFragment?.menuMarket!!){
             textViewTitle.text = getString(R.string.menu_market)
-            textViewNumber.text = getString(R.string.menu_number_03)
+            imageMenu.clipToOutline = true
+            imageMenu.loadImage("https://images.unsplash.com/photo-1621264448270-9ef00e88a935?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y3J5cHRvJTIwbWFya2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60")
             cardMenu.setOnClickListener()
             {
-                it.background = context?.getDrawable(R.color.primary)
+                //it.background = context?.getDrawable(R.color.primary)
                 UtilitiesFunction.replaceFragment(parentFragmentManager, MarketFragment())
             }
         }
         with(_getBindingHomeFragment?.menuExchanges!!){
             textViewTitle.text = getString(R.string.menu_exchanges)
-            textViewNumber.text = getString(R.string.menu_number_02)
+            imageMenu.clipToOutline = true
+            imageMenu.loadImage("https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNyeXB0byUyMG1hcmtldHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60")
             cardMenu.setOnClickListener {
-                it.background = context?.getDrawable(R.color.primary)
+                //it.background = context?.getDrawable(R.color.primary)
                 UtilitiesFunction.replaceFragment(parentFragmentManager, ExchangesFragment())
             }
         }
         with(_getBindingHomeFragment?.menuCoins!!){
             textViewTitle.text = getString(R.string.menu_coins)
-            textViewNumber.text = getString(R.string.menu_number_04)
+            imageMenu.clipToOutline = true
+            imageMenu.loadImage("https://images.unsplash.com/photo-1634024521600-0772a6b89fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")
             cardMenu.setOnClickListener {
-                it.background = context?.getDrawable(R.color.primary)
+                //it.background = context?.getDrawable(R.color.primary)
+                UtilitiesFunction.replaceFragment(parentFragmentManager, CoinsFragment())
+            }
+        }
+        with(_getBindingHomeFragment?.menuPriceConverter!!){
+            textViewTitle.text = getString(R.string.menu_price_converter)
+            imageMenu.clipToOutline = true
+            imageMenu.loadImage("https://images.unsplash.com/photo-1625208012722-1a8ab026b846?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTZ8fGV0aGVyZXVtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60")
+            cardMenu.setOnClickListener {
+                //it.background = context?.getDrawable(R.color.primary)
                 UtilitiesFunction.replaceFragment(parentFragmentManager, CoinsFragment())
             }
         }
